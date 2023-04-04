5781cc807b2b7e9da0de14a02baa05ba5feabaf3
louisnamb <63900979+louisnamb@users.noreply.github.com>
2023-04-04 16:22:37 +0930

    Add files via upload

--- exmaple.c
+++ exmaple.c
@@ -3,83 +3,71 @@
 # include <unistd.h>
 # include <stdio.h>
 
-int	ft_hex_putstr(char *s, int fd, int length, int ptr)
+int ft_hex_putstr(char *s, int fd, int length, int ptr)
 {
-	int	sum;
+    int sum = 0;
 
-	sum = 0;
-	if (!s)
-		return (0);
-	if (ptr)
-		sum += write(fd, "0x7ffe", 6);
-	while (length > 0)
+    if (!s)
+        return 0;
+    if (ptr)
+        sum += write(fd, "0x", 2);
+    printf("here\n");
+	while (length >= 0)
 	{
-		sum += write(fd, &s[length], 1);
-		length--;
+		sum += write(fd, s[length], length);
+
 	}
-	write(fd, &s[length], 1);
-	return (sum);
+    return sum;
 }
 
-int	ft_hex_length(int num, int base)
+int ft_hex_length(unsigned long num, int base)
 {
-	int	length;
-
-	length = 0;
-	while (num != 0)
-	{
-		num /= base;
-		length++;
-	}
-	return (length);
+    int length = 0;
+    while (num != 0)
+    {
+        num /= base;
+        length++;
+    }
+    return length;
 }
 
-int	ft_convert(unsigned int num, int base, int low, int ptr)
+int ft_convert(unsigned long num, int base, int low, int ptr)
 {
-	char			*uphexi;
-	char			*p_str;
-	char			*final;
-	int				chck;
-	unsigned int	dummy;
+    char *uphexi = "0123456789ABCDEF";
+    char *p_str;
+    char *final;
+    int chck = 0;
 
-	dummy = num;
-	chck = 0;
-	uphexi = "0123456789ABCDEF0123456789abcdef";
-	if (low)
-		chck = 16;
-	p_str = malloc(sizeof(char) * ft_hex_length(num, base) + 1);
-	if (!p_str)
-		return (0);
-	final = p_str;
-	while (dummy != 0)
-	{
-		*p_str = uphexi[dummy % base + chck];
-		dummy /= base;
-		p_str++;
-	}
-	*p_str = '\0';
-	return (ft_hex_putstr(final, 1, ft_hex_length(num, base), ptr));
+    if (low)
+        chck = 16;
+    p_str = malloc(sizeof(char) * ft_hex_length(num, base) + 1);
+    if (!p_str)
+        return 0;
+    final = p_str;
+    while (num != 0)
+    {
+        *p_str = uphexi[num % base + chck];
+        num /= base;
+        p_str++;
+    }
+    *p_str = '\0';
+    return ft_hex_putstr(final, 1, ft_hex_length(num, base), ptr);
 }
 
-unsigned int    ft_pointer(unsigned long n, int fd, int total, int *sum)
+unsigned int ft_pointer(unsigned long n, int fd, int total, int *sum)
 {
-	unsigned long	example = 3515;
-	void *s;
-	s = &example;
-	
-	unsigned long hex = (unsigned long)s;
-	printf("%lu", hex);
+    void *s = &n;
+    unsigned long hex = (unsigned long)s;
+    printf("%lu", hex);
     return (*sum + 1);
 }
 
 int main(void)
 {
-	unsigned int	example = 111321321;
-	void *s;
-	s = &example;
-	
-	unsigned int hex = (unsigned int)s;
-	printf("%p\n%u\n", s, hex);
-	ft_convert(hex, 16, 1, 1);
-	return 0;
+    unsigned int example = 111321321;
+    void *s = &example;
+    unsigned long hex = (unsigned long)s;
+    printf("%p\n%lu\n", s, hex);
+    ft_convert(hex, 16, 1, 1);
+    return 0;
 }
